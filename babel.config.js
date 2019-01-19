d0086969a7abc976b9157f866dce9e1643b4b99c
Infant Frontender <newbornfrontender@gmail.com>
2019-01-19 10:47:37 +0300

    Add minify preset

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,10 @@ module.exports = {
   presets: ['@babel/env'],
   plugins: ['@babel/proposal-class-properties'],
   sourceMaps: true,
+
+  env: {
+    production: {
+      presets: ['minify'],
+    },
+  },
 };
