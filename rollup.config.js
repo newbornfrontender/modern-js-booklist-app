8c5779ef5a9942c9255b9206d188c4b0278cb31f
Infant Frontender <newbornfrontender@gmail.com>
2019-01-19 11:22:30 +0300

    Remove rimraf

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
-import { sync as clean } from 'rimraf';
 import resolve from 'rollup-plugin-node-resolve';
 
 import babel from './plugins/babel';
 
 const production = !process.env.ROLLUP_WATCH;
-
-production && clean('public/*.{js,map}');
 
 export default {
   input: 'src/index.js',
